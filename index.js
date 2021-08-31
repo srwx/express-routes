fc63c500be721bc0134da6093dc09a68ddb133b1
Sorawong L <Sorawong2543@gmail.com>
2021-08-31 13:15:01 +0700

    add comment for Middleware

--- index.js
+++ index.js
@@ -4,13 +4,25 @@ const reqMethod = require("./middleware/reqMethod")
 const app = express()
 const PORT = process.env.PORT || 3000
 
-//Middleware
+// ------------------------------------------ Middleware ------------------------------------------
+// .use() คือการเรียกใช้ Middleware
+// Middleware จะทำงานทุกครั้ง เมื่อมี request ส่งมา
+// Middleware
 app.use(express.json()) //แปลง JSON String เป็น JSON Object (ทำให้รับข้อมูลมาแบบ JSON (ตอนรับข้อมูลด้วย req.body ใน POST method))
 app.use(express.urlencoded({ extended: false })) //แปลงข้อมูลจาก form เป็น Object
 
-//Custom Middleware
+// Custom Middleware
 app.use(reqMethod.start)
 app.use(reqMethod.check)
+// Middleware ด้านบน ไม่ได้ระบุ path ทำให้ทำงานทุกครั้งที่มีการส่ง request
+
+// Custom Middleware (with specfic path)
+app.use("/test/middleware", (req, res, next) => {
+  console.log(`Specfic middleware`)
+})
+// Middleware function นี้จะทำงานเมื่อ request path เป็น '/test/middleware' เท่านั้น
+
+// ------------------------------------------------------------------------------------------------
 
 app.use("/apis/resturants", resturantRouter) //resturant Router
 
--- middleware/reqMethod.js
+++ middleware/reqMethod.js
@@ -1,11 +1,13 @@
+// Middleware จะทำงานทุกๆครั้งเมื่อมี request ส่งมา
+
 function check(req, res, next) {
   console.log(`request method : ${req.method}`)
-  next()
+  next() // ทำงาน Middleware function ถัดไป
 }
 
 function start(req, res, next) {
   console.log("Hello")
-  next()
+  next() // ทำงาน Middleware function ถัดไป
 }
 
 module.exports = { check, start }
